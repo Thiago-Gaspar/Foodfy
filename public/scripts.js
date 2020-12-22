27de1037bc8d86ddb021123a3fc0af2be4d4cfd4
Thiago Gaspar <thiagogasparr@gmail.com>
2020-12-22 19:55:42 -0300

    Começando a refatorar o Foodfy

---
+++ public/scripts.js
@@ -0,0 +1,25 @@
+const modalOverlay = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.card')
+
+
+
+for(let card of cards) {
+    card.addEventListener('click', function() {
+    let img = card.querySelector('img').src
+    let h1 = card.querySelector('h1').innerHTML
+    let p = card.querySelector('p').innerHTML
+    modalOverlay.classList.add('active')
+    modalOverlay.querySelector('.card_image img').src = img
+    modalOverlay.querySelector('.card_title h1').innerHTML = h1
+    modalOverlay.querySelector('.card_author p').innerHTML = p
+    
+    })
+}
+
+const close = document.querySelector('.close')
+close.addEventListener('click', function () {
+    modalOverlay.classList.remove('active')
+})
+
+
+
